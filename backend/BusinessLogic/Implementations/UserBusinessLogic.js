8ea0c32e9eb804089677c83a9f71b5636763e6f9
Midas van Oene <midas.van.oene@gmail.com>
2018-02-11 15:57:52 +0100

    delete generated files

--- backend/BusinessLogic/Implementations/UserBusinessLogic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserBusinessLogic {
-    getUsers() {
-        throw new Error("Method not implemented.");
-    }
-}
-exports.UserBusinessLogic = UserBusinessLogic;
-//# sourceMappingURL=UserBusinessLogic.js.map
--- backend/DataLogic/Implementations/UserDataLogic.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class UserDataLogic {
-    getUsers() {
-        throw new Error("Method not implemented.");
-    }
-}
-exports.UserDataLogic = UserDataLogic;
-//# sourceMappingURL=UserDataLogic.js.map
